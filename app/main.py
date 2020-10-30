85abceefd4f25c18d1bd7507f5857b383de26dab
qu3nt1n@gmail.com <qu3nt1n@gmail.com>
2020-10-30 15:06:43 +0100

    new paths

--- app/main.py
+++ app/main.py
@@ -64,7 +64,7 @@ def read_pdf_file(filepath):
     return return_data
 
 
-TEXT_PATH = "app/text/attestation-de-deplacement-derogatoire.md"
+TEXT_PATH = "/app/app/text/attestation-de-deplacement-derogatoire.md"
 TEXT = get_text(TEXT_PATH)
 LISTE_CHAMPS = ['nom',
                 'prenom',
@@ -83,7 +83,7 @@ MOTIFS = ['travail',
           'justice',
           'autorite',
           'enfants']
-OUTPUT_PDF = "app/pdf/attestation.pdf"
+OUTPUT_PDF = "/app/app/pdf/attestation.pdf"
 
 # Flask
 app = Flask(__name__)
